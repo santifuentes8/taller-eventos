e3422a1d2d8d71f4815318f3121e892620c7a83b
santifuentes8 <santagus12@gmail.com>
2023-08-13 14:21:04 -0300

    Solucion al problema del click

--- eventos.js
+++ eventos.js
@@ -1,6 +1,12 @@
 let div= document.querySelector('div');
+let btn=document.querySelector('button');
+
 div.addEventListener('click',()=>{
     alert('Hola! Soy el div');}
 )
   ;
+  btn.addEventListener('click', (x)=>{
+    alert('Hola!');
+    x.stopPropagation();
+  })
 
